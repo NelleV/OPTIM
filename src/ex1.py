1b0c2a5277fa4de876dbdc3d09033cb6de0d9bbd
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2012-03-04 11:02:48 +0100

    EXO 1

--- src/ex1.py
+++ src/ex1.py
@@ -1,56 +1,93 @@
 import numpy as np
+import sys
 
-m = 10
-n = 7
+
+# backtracking line search
+def backtracking_line_search(X, delta, G):
+    alpha = 0.3
+    beta = 0.5
+    t = 1
+    FX = - np.log((B - (A * X).sum(axis=1)).sum(axis=0))
+    Y = X + t * delta
+    FY = - np.log((B - (A * Y).sum(axis=1)).sum(axis=0))
+    # Check that X is in the domain
+    while np.isnan(FY) > 0 or \
+          FY > FX + alpha * t * np.dot(G.T, delta):
+
+        print FY.sum(), (FX + alpha * t * np.dot(G.T, delta)).sum()
+        Y = X + t * delta
+        FY = - np.log((B - (A * Y).sum(axis=1)).sum(axis=0))
+        t = beta * t
+    return t
+
+m = 2
+n = 2
 # m * n matrice
-A = np.random.randint(1000, size=(m, n)).astype(float)
-B = np.random.randint(1000, size=(m, 1)).astype(float)
+A = np.random.randint(100, size=(m, n)) * np.random.random(size=(m, n))
+B = np.random.randint(1000, size=(m, )).astype(float) * \
+    np.random.random(size=(m, ))
+
+
+def compute_gradient(X, B, A):
+    # Compute gradient G
+    det = (B - (A * X).sum(axis=1))
+    det = det.repeat(n).reshape((m, n))
+    a = A / det
+    G = a.sum(axis=0)
+    return G
+
+
+def compute_hessian(X, B, A):
+    # Compute gradient G
+    det = (B - (A * X).sum(axis=1))
+    det = det.repeat(n).reshape((m, n))
+    a = A / det
+    H = np.dot(a.T, a)
+    return H
+
 
 eps = 10e-10
 t = 1
 delta = np.zeros((n, ))
-max_iter = 300
+max_iter = 100
 verbose = True
 
-# Choose random point in dom F
-X = np.random.randint(100, size=(n, )).astype(float)
+X = - np.random.random(size=(n, )).astype(float)
 
+logfile = sys.stdout
 # Keep track of all the computed values of X, in order to plot convergence.
 values = []
+FXs = []
 for niter in range(max_iter):
+    if verbose:
+        logfile.write('\r %d %%' % (float(niter + 1) / max_iter * 100))
+        logfile.flush()
     X += t * delta
-    values.append(X)
-    B = B.repeat(n, axis=1)
+    values.append(X.copy())
+    FX = - np.log((B - (A * X).sum(axis=1)).sum(axis=0))
+    FXs.append(FX)
     # Compute Newton's step
-
-    # Compute gradient G
-    G = (1. / (B - A * X)).sum(axis=0)
+    G = compute_gradient(X, B, A)
+    H = compute_hessian(X, B, A)
 
     # Compute Hessian H
-    # FIXME that is so wrong...
-    H = np.identity(n)
-    H *= (- A / (B - A * X) ** 2).sum(axis=0)
+    L = np.linalg.cholesky(H)
+    L_inv = np.linalg.inv(L)
 
-    delta = - np.dot(np.linalg.inv(H), G)
-    lambda2 = np.dot(np.dot(G.T, np.linalg.inv(H)), G)
-
+    delta = - np.dot(np.dot(L_inv.T, L_inv), G)
+    lambda2 = (np.dot(L_inv, G) ** 2).sum()
+    assert lambda2 > 0
     if lambda2 / 2 < eps:
         if verbose:
             print "Break out of the loop at %d" % niter
         break
 
     # TODO implement the backtracking line search to choose t
+    t = backtracking_line_search(X.copy(), delta.copy(), G.copy())
 
 # Now that we have an optimal X, let's compute the distance of values to X
-values = np.array(values)
 X_opt = X.repeat(len(values))
+X_opt = X_opt.reshape((n, len(values))).T
 distances = ((values - X_opt) ** 2).sum(axis=1)
 
 # TODO plot the convergence curve
-
-# backtracking line search
-alpha = 0.25
-beta = 0.5
-t = 1
-Y = X + t * delta
-F = np.log((B - A * X).sum(axis=0))
